38c0f93dd2de8e552fd9fd42ce51bb8e158801e2
Unknown <patrick.mueller@tu-ilmenau.de>
2017-05-17 15:05:17 +0200

    Added Tools for database interaction
    added an eloquent model and a new controller class for interacting with database guest_attendance_list

---
+++ app/GuestListAttendanceList.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lara;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GuestListAttendanceList extends Model
+{
+    
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'guest_attendance_list';
+
+    /**
+     * The database columns used by the model.
+     * This attributes are mass assignable.
+     *
+     * @var array
+     */
+     protected fillable
+}
