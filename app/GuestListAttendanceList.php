ef5bc1c4a3430e916ed5624dd1024d50919e8cc9
Jonas Willmann <jonas.willmann@tu-ilmenau.de>
2017-05-30 15:58:13 +0200

    Merge branch 'Patrick_dev' into jonas_dev

---
+++ app/GuestListAttendanceList.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Lara;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GuestListAttendanceList extends Model
+{
+    
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'guest_attendance_list';
+
+    /**
+     * The database columns used by the model.
+     * This attributes are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = array('personidclub',
+                                 'personid',
+                                 'name',
+                                 'surname',
+                                 'status',
+                                 'comment',
+                                 'importsource',    //0->added manually
+                                                    //1->added by clubleitung
+                                                    //2->added by member
+                                                    //3->added by admin
+                                                    //->...
+                                                    //4->added via facebook import
+                                 'attendancestatus',
+                                 'eventid');
+
+//name functions correctly and get data
+public function getUserID() {
+		return $this->belongsTo('Lara\users', 'personid', 'id');
+	}
+
+public function getEventID() {
+        return $this->belongsTo('Lara\club_events', 'eventid', 'id');
+    }
+
+
+}
---
+++ app/Http/Controllers/GuestListAttendanceListController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Lara\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Lara\Http\Requests;
+use Lara\Http\Controllers\Controller;
+
+use Lara\Utilities;
+use Session;
+use Cache;
+use DateTime;                       //timestamp
+use DateInterval;                   //timestamp
+use DateTimeZone;                   //timestamp
+use View;
+use Input;
+use Config;
+use Log;
+use Redirect;
+
+use Carbon\Carbon;
+
+use Lara\ClubEvent;
+use Lara\Schedule;
+use Lara\ScheduleEntry;
+use Lara\Jobtype;
+use Lara\Person;
+use Lara\Club;
+use Lara\Place;
+use Lara\GuestListAttendanceList;
+
+class GuestListAttendanceListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Not showing everything to guests or managing reading and writing rules
+        //currently not showing content to guests
+        if(!Session::has('userId'))
+        {
+            Session::put('message', Config::get('messages_de.access-denied'));
+            Session::put('msgType', 'danger');
+            return Redirect::action('MonthController@showMonth', ['year' => date("Y"), 'month' => date('m')]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($templateId = null)
+    {
+        
+        if ( is_null($templateId) ) {
+            $templateId = 0;    // 0 = no template
+        }
+
+        
+        // if a template id was provided, load the schedule needed and extract job types
+        if ( $templateId != 0 ) {
+            $template = Schedule::where('id', '=', $templateId)
+                                ->first();
+
+            // put name of the active template for further use
+            $activeTemplate = $template->guest_attendance_list;
+            
+            //get template
+            $personidclub       = $template->getGuestListAttendanceList->personidclub;
+            $personid           = $template->getGuestListAttendanceList->personid;
+            $name               = $template->getGuestListAttendanceList->name;
+            $surname            = $template->getGuestListAttendanceList->surname;
+            $status             = $template->getGuestListAttendanceList->status;
+            $comment            = $template->getGuestListAttendanceList->comment;
+            $importsource       = $template->getGuestListAttendanceList->importsource;    //?
+            $attendancestatus   = $template->getGuestListAttendanceList->attendancestatus;
+            $eventid            = $template->getGuestListAttendanceList->eventid;           //get it from event page
+
+        } else {
+            // fill variables with no data if no template was chosen, but not sure if needed here
+            $activeTemplate = "";
+            $personidclub       = null;
+            $personid           = null;
+            $name               = null;
+            $surname            = null;
+            $status             = null;
+            $comment            = null;
+            $importsource       = null;
+            $attendancestatus   = null;
+            $eventid            = null;      //maybe get eventid from event page
+        }
+                
+        //return values for creating new table entry
+        return View::make('createGuestAttendanceList', compact('personidclub','personid','name','surname',
+                                                               'status','comment','importsource',
+                                                               'attendancestatus','eventid'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /*
+
+        */
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        /*
+
+        */
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        /*
+            //edit existing entrys
+        */
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //here could be the ajax server stuff, but not sure ask patche if possible
+        /*
+
+        */
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //to destroy an entry
+    }
+}
