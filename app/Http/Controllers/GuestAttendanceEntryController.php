6d98622a2a28a954bdb721cc510297f56f28fd0c
Patrick Müller <patrick.mueller@tu-ilmenau.de>
2017-06-13 23:38:50 +0200

    [Controller]Add dependencies for usage
    Added use with everything that might be needed in the controller

--- app/Http/Controllers/GuestAttendanceEntryController.php
+++ app/Http/Controllers/GuestAttendanceEntryController.php
@@ -2,8 +2,33 @@
 
 namespace Lara\Http\Controllers;
 
-use Lara\GuestListAttendanceList;
 use Illuminate\Http\Request;
+
+use Lara\Http\Requests;
+use Lara\Http\Controllers\Controller;
+
+use Lara\Utilities;
+use Session;
+use Cache;
+use DateTime;                       //timestamp
+use DateInterval;                   //timestamp
+use DateTimeZone;                   //timestamp
+use View;
+use Input;
+use Config;
+use Log;
+use Redirect;
+
+use Carbon\Carbon;
+
+use Lara\ClubEvent;
+use Lara\Schedule;
+use Lara\ScheduleEntry;
+use Lara\Jobtype;
+use Lara\Person;
+use Lara\Club;
+use Lara\Place;
+use Lara\GuestListAttendanceList;
 
 class GuestAttendanceEntryController extends Controller
 {
