c1cbf908d39917e565de9485f6f04aef9d8d6c7b
Jonas Willmann <jonas.willmann@tu-ilmenau.de>
2017-06-13 17:09:15 +0200

    Revert "Revert "[Guestlist] Edit Guestlist View""
    This reverts commit 4ca4deb65c8543736f4c3e34406523c6f2d74d58.

--- resources/views/partials/clubEventGuestlist.blade.php
+++ resources/views/partials/clubEventGuestlist.blade.php
@@ -1,136 +1,27 @@
 Guestlist to be integrated here.
-@if($guestlistattendancelist->eventid != '')
+@if($guestlistattendancelist->evnt_id != '')
 
 <div class="panel panel-warning">
 
-
 	<div class="panel-body no-padding">
-		<span hidden>{{$counter = 0}}</span>
+	
+		{!! Form::open( array('method' => 'PUT',
+		   					  'route' => ['guestentry.update', $guestlistattendancelist->id],
+							  'id' => $guestlistattendancelist->id)) !!}
+
+			{!! Form::text(	'name' . $guestlistattendancelist->id,
+							$guestlistattendancelist->name,
+							array(	'placeholder'=>Lang::get('mainLang.firstname'),
+									'id'=>'name' . $guestlistattendancelist->id,
+									'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+			!!}
+
+		           
+
+			
+		{!! Form::close() !!}	
+			
 		
-			<div class="row pattingTop">
-
-				<div id="container" class="container">
- {{-- If there are entries passed - fill them with data and increment counter --}} 
-
-		   		{!! Form::open(['method' => 'PUT', 'route' => ['guestentry.update', $counter]]) !!}
-
-		            <div id={{ "box" . ++$counter }} class="box">
-
-					
-
-						<input type="text" 
-				       		   name={{ "name" . $counter }}
-				       		   class="input" 
-			           		   id={{ "name" . $counter }}
-			           		   value=""
-			           		   placeholder="{{ trans('mainLang.firstname') }}"/>          					           	
-						
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-						<input type="text" 
-				           	   name={{ "surname" . $counter }}
-				           	   class="input" 
-				        	   id={{ "surname" . $counter }}
-				       		   value=""
-				        	   placeholder="{{ trans('mainLang.surname') }}"/>
-
-					{{--  CHECKBOX  --}}
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          			   <label class="custom-control custom-checkbox">
-            		  	 <input type="checkbox" class="custom-control-input">
-                      	 <span class="custom-control-indicator"></span>
-                      	 <span class="custom-control-description"></span>
-                       </label>
-					   <label class="custom-control custom-checkbox">
-            		  	 <input type="checkbox" class="custom-control-input">
-                      	 <span class="custom-control-indicator"></span>
-                      	 <span class="custom-control-description"></span>
-                       </label>
-
-
-
-		            	&nbsp;&nbsp;&nbsp;&nbsp;
-		            	<input type="text" 
-		            		   class="input" 
-		            		   name={{ "addComment" . $counter }} 
-		            		   id={{ "addComment" . $counter }}
-		            		   value=""
-							   placeholder="{{ trans('mainLang.addComment') }}" />
-							   
-
-		            	
-		            	<input type="button" value="+" class="btn btn-small btn-success btnAdd" />
-		            	&nbsp;&nbsp;
-	    				@if(Session::has('userId'))
-						<input type="button" value="&#8211;" class="btn btn-small btn-danger btnRemove" />
-						@endif
-					</div>
-					
-				
-			
-
-
-		    {{-- and add one empty entry --}}
-		       <div id={{ "box" . ++$counter }} class="box">
-			           	
-				    <input type="text" 
-				    	   name={{ "name" . $counter }}
-			       		   class="input" 
-		           		   id={{ "name" . $counter }}
-		           		   value=""
-		           		   placeholder="{{ trans('mainLang.firstname') }}"/>
-				           	
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				
-
-					<input type="text" 
-		           		   name={{ "surname" . $counter }}
-		           		   class="input" 
-		           		   id={{ "surname" . $counter }}
-		           		   value=""
-		           		   placeholder="{{ trans('mainLang.surname') }}"/>
-
-
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	        	<label class="custom-control custom-checkbox">
-            		<input type="checkbox" class="custom-control-input">
-                    <span class="custom-control-indicator"></span>
-                	<span class="custom-control-description"></span>
-                </label> 
-				<label class="custom-control custom-checkbox">
-            		<input type="checkbox" class="custom-control-input">
-                    <span class="custom-control-indicator"></span>
-                	<span class="custom-control-description"></span>
-                </label> 
-
-         	&nbsp;&nbsp;&nbsp;&nbsp;
-		        <input type="text" 
-		        	   class="input" 
-		    		   name={{ "addComment" . $counter }} 
-            		   id={{ "addComment" . $counter }}
-		        	   value=""
-					   placeholder="{{ trans('mainLang.addComment') }}" />    
-							             
-
-
-
-	        	<input type="button" value="+" class="btn btn-small btn-success btnAdd" /> 
-	        	&nbsp;&nbsp;
-				@if(Session::has('userId'))
-				<input type="button" value="&#8211;" class="btn btn-small btn-danger btnRemove" />
-				@endif
-				
-			
-	    	</div>
-	    	<br>
-			<input type="hidden" name="counter" id="counter" value="{{$counter}}" />
-			{!! Form::submit('Eintrag speichern', array('class'=>'hidden', 'id'=>'button-create-submit')) !!}
-			<button class="btn btn-primary" id="saveGuestentry"></button>
-			
-			{!! Form::close() !!}	
-			
-			</div>		
-		</div>
 	</div>
 </div>
 @endif
