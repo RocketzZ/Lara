63243bda7eea8efe7244707c20d68f25d5b4389f
Patrick Müller <patrick.mueller@tu-ilmenau.de>
2017-06-13 18:21:42 +0200

    [Controller]add Controller for simpler interaction
    Added GuestAttendanceEntryController.php like ScheduleEntryController.php for mor simple interaction between backend and frontend

---
+++ app/Http/Controllers/GuestAttendanceEntryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Lara\Http\Controllers;
+
+use Lara\GuestListAttendanceList;
+use Illuminate\Http\Request;
+
+class GuestAttendanceEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Lara\GuestListAttendanceList  $guestListAttendanceList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GuestListAttendanceList $guestListAttendanceList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Lara\GuestListAttendanceList  $guestListAttendanceList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GuestListAttendanceList $guestListAttendanceList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Lara\GuestListAttendanceList  $guestListAttendanceList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GuestListAttendanceList $guestListAttendanceList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Lara\GuestListAttendanceList  $guestListAttendanceList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GuestListAttendanceList $guestListAttendanceList)
+    {
+        //
+    }
+}
