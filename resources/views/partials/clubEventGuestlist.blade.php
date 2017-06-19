4a048742a2d2624d6370963ae053008f68fb4e4e
Jonas Willmann <jonas.willmann@tu-ilmenau.de>
2017-06-19 13:21:24 +0200

    [Guestlist] Add new Forms
    Added Forms to clubEventGuestlist.blade.php for Surname and Comment.

--- resources/views/partials/clubEventGuestlist.blade.php
+++ resources/views/partials/clubEventGuestlist.blade.php
@@ -4,24 +4,39 @@ Guestlist to be integrated here.
 <div class="panel panel-warning">
 
 	<div class="panel-body no-padding">
-	
+		<div class="row paddingTop">
+
 		{!! Form::open( array('method' => 'PUT',
 		   					  'route' => ['guestentry.update', $guestlistattendancelist->id],
 							  'id' => $guestlistattendancelist->id)) !!}
-
+			
+			
 			{!! Form::text(	'name' . $guestlistattendancelist->id,
 							$guestlistattendancelist->name,
 							array(	'placeholder'=>Lang::get('mainLang.firstname'),
 									'id'=>'name' . $guestlistattendancelist->id,
 									'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
 			!!}
-
+			&nbsp;&nbsp;
+			{!! Form::text(	'surname' . $guestlistattendancelist->id,
+							$guestlistattendancelist->surname,
+							array(	'placeholder'=>Lang::get('mainLang.surname'),
+									'id'=>'surname' . $guestlistattendancelist->id,
+									'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+			!!}
+			&nbsp;&nbsp;
+			{!! Form::text(	'comment' . $guestlistattendancelist->id,
+							$guestlistattendancelist->comment,
+							array(	'placeholder'=>Lang::get('mainLang.addComment'),
+									'id'=>'comment' . $guestlistattendancelist->id,
+									'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+			!!}
 		           
 
 			
 		{!! Form::close() !!}	
 			
-		
+		</div>	
 	</div>
 </div>
 @endif
