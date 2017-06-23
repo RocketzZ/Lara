282e31adaff1f83544205768dc4b75708cf67ed2
Unknown <rehlinger.yann@hotmail.com>
2017-06-23 14:47:16 +0200

    clubEventAttendanceStatistic header
    added header for attendancestatistic
    included attendance statistic into attendance list

---
+++ resources/views/partials/clubAttendanceStatistic.blade.php
@@ -0,0 +1,6 @@
+<div class="panel">
+    <div class="panel-heading">
+        <h4 class="panel-title">{{ trans('mainLang.AttendanceStatisticHeader') }}:</h4>
+    </div>
+
+    <div class="panel-body no-padding" id="main">
