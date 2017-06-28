629d93c1b219da83fc51922d2eaeee4138871b69
Jonas Willmann <jonas.willmann@tu-ilmenau.de>
2017-06-28 17:47:02 +0200

    [Guestlist] Reverted Forms
    Reverted the added Forms for an empty Guestentry as they did not work properly when creating a new Event.

--- resources/views/partials/clubEventGuestlist.blade.php
+++ resources/views/partials/clubEventGuestlist.blade.php
@@ -75,57 +75,7 @@
 
 		{{--Add one empty Field at the End--}}
 			
-			<span hidden>{{$guestentry->id = ++$guestentry->id}}</span>
 			
-			{!! Form::open( array('method' => 'PUT',
-		   						  'route' => ['guestentry.update', $guestentry->id],
-								  'id' => $guestentry->id,
-								  'class' => 'GuestListAttendanceList'))
-			!!}
-			
-			{{-- Firstname and Surname --}}
-
-				<div class="col-md-2 col-sm-2 col-xs-3 no-padding">
-					{!! Form::text(	'name' . $guestentry->id,
-									Input::old('name' . $guestentry->name),
-									array(	'placeholder'=>Lang::get('mainLang.firstname'),
-											'id'=>'name' . $guestentry->id,
-											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
-					!!}
-				</div>
-				&nbsp;&nbsp;
-
-				<div class="col-md-2 col-sm-2 col-xs-3 no-padding">
-					{!! Form::text(	'surname' . $guestentry->id,
-									Input::old('surname' . $guestentry->surname),
-									array(	'placeholder'=>Lang::get('mainLang.surname'),
-											'id'=>'surname' . $guestentry->id,
-											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
-					!!}
-				</div>
-
-				&nbsp;&nbsp;
-
-			{{--Comment Section --}}
-
-				<div class="col-md-6 col-sm-12 col-xs-12 no-margin">
-					<span class="pull-left">
-						{!! $guestentry->comment == "" ? '<i class="fa fa-comment-o"></i>' : '<i class="fa fa-comment"></i>' !!}
-						&nbsp;&nbsp;
-					</span>
-
-					{!! Form::text(	'comment' . $guestentry->id,
-									Input::old('comment' . $guestentry->comment),
-									array(	'placeholder'=>Lang::get('mainLang.addComment'),
-											'id'=>'comment' . $guestentry->id,
-											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
-					!!}
-			
-				</div>				
-				
-				{!! Form::submit('save', array('id'=>'btn-submit-changes' . $guestentry->id, 'hidden')) !!}
-			
-			{!! Form::close() !!}
 
 		</div>
 	</div>
