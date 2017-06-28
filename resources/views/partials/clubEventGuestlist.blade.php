d648419a637fd34d20f7a857e795146f680c698b
Jonas Willmann <jonas.willmann@tu-ilmenau.de>
2017-06-28 17:21:30 +0200

    [Guestlist] Changed Submit Forms
    Changed Submit Forms in GuestList View, only allow logged in Members to change Guestentries.

--- resources/views/partials/clubEventGuestlist.blade.php
+++ resources/views/partials/clubEventGuestlist.blade.php
@@ -5,18 +5,15 @@
 	<div class="panel-body no-padding">
 
 		<div class="row paddingTop">
-		
-			
-			
-			
-			
-			@foreach($guestentry as $guestentry)
 
-			{{-- Add new Guestentries --}}
+		{{-- Show all Guestentries --}}
+
+			@foreach($guestentry as $guestentry)
 		
 			{!! Form::open( array('method' => 'PUT',
 		   						  'route' => ['guestentry.update', $guestentry->id],
-								  'id' => $guestentry->id,))
+								  'id' => $guestentry->id,
+								  'class' => 'GuestListAttendanceList'))
 			!!}
 			
 			{{-- Firstname and Surname --}}
@@ -60,17 +57,73 @@
 				</div>
 
 				<label for="attendancestatus">{{ trans('mainLang.Attendancestatus') }}</label>
+				
 				{!! Form::checkbox(	'attendancestatus' . $guestentry->id,
 									$guestentry->attendancestatus,
 									'0', false)
 				!!} 
+			
+			{{--Only allow logged in Members to change Guestentries--}}
 
-				{!! Form::submit('Eintrag speichern', array('class'=>'hidden', 'id'=>'button-create-submit')) !!} 
-      			<button class="btn btn-primary" id="saveGuestentry">{{trans('mainLang.Save')}}</button> 
+				@if (Session::has('userID'))
+					{!! Form::submit('Eintrag speichern', array('class'=>'hidden')) !!}
+				@endif
 
 			{!! Form::close() !!}
 
 			@endforeach
+
+		{{--Add one empty Field at the End--}}
+
+			{!! Form::open( array('method' => 'PUT',
+		   						  'route' => ['guestentry.update', $guestentry->id],
+								  'id' => $guestentry->id,
+								  'class' => 'GuestListAttendanceList'))
+			!!}
+			
+			{{-- Firstname and Surname --}}
+
+				<div class="col-md-2 col-sm-2 col-xs-3 no-padding">
+					{!! Form::text(	'name' . $guestentry->id,
+									Input::old('name' . $guestentry->name),
+									array(	'placeholder'=>Lang::get('mainLang.firstname'),
+											'id'=>'name' . $guestentry->id,
+											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+					!!}
+				</div>
+				&nbsp;&nbsp;
+
+				<div class="col-md-2 col-sm-2 col-xs-3 no-padding">
+					{!! Form::text(	'surname' . $guestentry->id,
+									Input::old('surname' . $guestentry->surname),
+									array(	'placeholder'=>Lang::get('mainLang.surname'),
+											'id'=>'surname' . $guestentry->id,
+											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+					!!}
+				</div>
+
+				&nbsp;&nbsp;
+
+			{{--Comment Section --}}
+
+				<div class="col-md-6 col-sm-12 col-xs-12 no-margin">
+					<span class="pull-left">
+						{!! $guestentry->comment == "" ? '<i class="fa fa-comment-o"></i>' : '<i class="fa fa-comment"></i>' !!}
+						&nbsp;&nbsp;
+					</span>
+
+					{!! Form::text(	'comment' . $guestentry->id,
+									Input::old('comment' . $guestentry->comment),
+									array(	'placeholder'=>Lang::get('mainLang.addComment'),
+											'id'=>'comment' . $guestentry->id,
+											'class'=>'col-md-11 col-sm-11 col-xs-10 no-padding no-margin'))
+					!!}
+			
+				</div>				
+				
+				{!! Form::submit( 'Eintrag speichern', array('class'=>'hidden')) !!}
+			
+			{!! Form::close() !!}
 
 		</div>
 	</div>
